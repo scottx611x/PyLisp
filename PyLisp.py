fc27ed52bbba3a16399bae57ce41580be1b2ccc1
Scott Ouellette <Scott@Scotts-MacBook-Pro.local>
2015-07-27 00:18:06 -0400

    input formatting

--- PyLisp.py
+++ PyLisp.py
@@ -1,22 +1,29 @@
-import re
-import operator
+import re, operator
 
 
 #PyLisp 
-#interpreter for Common Lisp written in python
+#interpreter for Lisp written in python
 
 #Sample Data
-line = "( * ( 2 ) ( - 130 2 ) ( + 1 1 1 1 ) ( / 696969696969696969696969 696969696969696969696969 ) )"
+line = "(* (2)(- 130 2)(+ 1 1 1 1)(/ 9745939781548 101912))"
+
+#Do formatting on bad input data
+validOps = ["+", "-", "*", "/", "%"]
+for validOp in validOps:
+    if validOp in line:
+        line = line.replace("(%s" % validOp, "( %s" % validOp)
+        line = line.replace("%s(" % validOp, "%s (" % validOp)
+line = re.sub(r"(\d)(\))", r"\1 \2", line)
+line = re.sub(r"(\()(\d)", r"\1 \2", line)
+rep = {"((" : "( (", "))" : ") )", ")(" : ") ("}
+rep = dict((re.escape(k), v) for k, v in rep.iteritems())
+pattern = re.compile("|".join(rep.keys()))
+line = pattern.sub(lambda m: rep[re.escape(m.group(0))], line)
 
 #Reducer: takes an operator i.e. "+" and a list of ints to reduce
 def reducer(operation, listOfNumbers):
-
-    #getOperator: takes the operator from reducer and returns the mathematical function it entails
-    def getOperator(operation):
-
-        return {'+' : operator.add,'-' : operator.sub,'*' : operator.mul,'/' : operator.div,'%' : operator.mod,}[operation]
-
-    return reduce(lambda x, y: (getOperator(operation)(x,y)), listOfNumbers)
+    op = {'+' : operator.add,'-' : operator.sub,'*' : operator.mul,'/' : operator.div, '%' : operator.mod,}[operation]
+    return reduce(lambda x, y: (op(x,y)), listOfNumbers)
 
 #Interpret a line of Lisp
 def process_line(line):
@@ -27,30 +34,21 @@ def process_line(line):
 
     #Iterate through list of Lisp code
     for index, x in enumerate(line.split()):
-
         if x in ["(", "+", "-", "*", "/", "%"]:
             opstack.append(x)
-
         if x.isdigit():
             operandStack.append(int(x)) 
-
         if x == ")":
-
+            
             #If we get to the last ')'
             if index == (len(line.split()) - 1) - line.split()[::-1].index(')'):
-                
                 for item in finalOperands:
                     operands.append(item)
-
                 op = opstack.pop()
-        
                 print "LISP EXPRESSION: ", line , "\n\nEvaluates to: ", reducer(op, operands), "\n"
-
                 break
-
             for item in operandStack:
                 operands.append(item)
-            
             operandStack = []
 
             #Get operation
@@ -59,16 +57,12 @@ def process_line(line):
             #Account for single nums in parens => ( 3 )
             if not op in ["+", "-", "*", "/", "%"]:
                 finalOperands.append(operands[0])   
-
             else:
-
+                
                 #Get rid of '('
                 opstack.pop()
-                
+
                 #Append inner result to finalOperands
-
                 finalOperands.append(reducer(op, operands))
-
             operands = []
-
 process_line(line)
